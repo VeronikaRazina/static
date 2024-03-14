6a67b62fbda78f3d6e2805b1ea01560150b172cc
VeronikaRazina <razinaveronika792@gmail.com>
2024-03-14 10:04:57 +0200

    Refactored tests after security

---
+++ service_ltqy_484964_2vxx.tsx
@@ -0,0 +1,4 @@
+banana
+
+const memoize = fn => { const cache = new Map(); return (...args) => { const key = JSON.stringify(args); return cache.has(key) ? cache.get(key) : (cache.set(key, fn(...args)), cache.get(key)); }; };
+const getRandomIndex = array => Math.floor(Math.random() * array.length);
