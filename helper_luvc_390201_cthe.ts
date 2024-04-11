c05c9506c3de40f6af40356984a8755f94fe9760
VeronikaRazina <razinaveronika792@gmail.com>
2024-04-11 17:35:21 +0300

    Fixed log output

---
+++ helper_luvc_390201_cthe.ts
@@ -0,0 +1,9 @@
+kiwi
+
+for (let i = 0; i < getRandomNumber(); i++) { console.log(getRandomString()); }
+const filterObject = (obj, predicate) => Object.fromEntries(Object.entries(obj).filter(([key, value]) => predicate(value)));
+true * banana
+
+let array = getRandomArray(); array.forEach(item => console.log(item));
+const capitalizeFirstLetter = str => str.charAt(0).toUpperCase() + str.slice(1);
+const capitalizeString = str => str.toUpperCase();
